1b8a8b157c8c9e6c48779a374ed5fcd000fc3297
amabeyiu <galuhshalira@gmail.com>
2025-11-29 23:15:42 +0700

    TP MODUL 5 (Galuh)

--- WAD_REDA_TP5/routes/api.php
+++ WAD_REDA_TP5/routes/api.php
@@ -4,13 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\CategoriesController;
+use Illuminate\Http\Request;
 
 /**
  * ==========1===========
  * unprotected routes for user registration and login
  */
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 /**
  * =========2===========
@@ -21,7 +22,10 @@ Route::middleware('auth:sanctum')->group(function () {
      * =========3===========
      * User logout route
      */
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
 
     /**
      * =========4===========
